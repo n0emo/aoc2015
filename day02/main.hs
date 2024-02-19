e9253be63e77a203349bb708f37d6c71e9fe37c0
Albert Shefner <solaris945@gmail.com>
2024-02-19 23:35:16 +0300

    Solve day 2

---
+++ day02/main.hs
@@ -0,0 +1,45 @@
+import System.IO
+
+splitBy :: (Char -> Bool) -> String -> [String]
+splitBy p s = case dropWhile p s of
+  "" -> []
+  s' -> w : splitBy p s''
+    where
+      (w, s'') = break p s'
+
+parseBox :: String -> (Int, Int, Int)
+parseBox str = case map read $ splitBy (== 'x') str of
+  [a, b, c] -> (a, b, c)
+  _badInput -> error "Bad input"
+
+evalBoxPaper (l, w, h) =
+  sum
+    [ 2 * sideA,
+      2 * sideB,
+      2 * sideC,
+      minimum [sideA, sideB, sideC]
+    ]
+  where
+    sideA = l * w
+    sideB = w * h
+    sideC = h * l
+
+evalBoxRibbon (l, w, h) = minimum [p l w, p l h, p w h] + l * w * h
+  where
+    p a b = a + a + b + b
+
+main :: IO ()
+main = do
+  file <- openFile "input.txt" ReadMode
+  contents <- hGetContents file
+  let boxes = lines contents
+  let paper = eval evalBoxPaper boxes
+  let ribbon = eval evalBoxRibbon boxes
+
+  putStr "Wrapping paper: "
+  print paper
+
+  putStr "Ribbon: "
+  print ribbon
+  where
+    eval f boxes = sum $ map (f . parseBox) boxes
